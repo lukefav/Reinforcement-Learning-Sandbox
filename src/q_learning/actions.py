a18fe803830d06d1a32de0df61182db425ad2bde
lukefav <luke.favr@gmail.com>
2020-06-15 19:11:59 -0400

    Epsilon implementation complete

--- src/q_learning/actions.py
+++
@@ -1,7 +0,0 @@
-import enum
-
-
-class Actions(enum.Enum):
-    Left = 0,
-    Nothing = 1,
-    Right = 2
--- src/q_learning/q_table.py
+++ src/q_learning/q_table.py
@@ -8,14 +8,14 @@ class QTable:
     _DISCOUNT = 0.95  # Measure of how important we find future rewards over current rewards
 
     _INITIAL_EPSILON = 0.5  # Measure of how much randomness included in decision making (exploration)
-    _EPSILON_DECAY_VALUE = 0.01
     _BEGIN_EPSILON_DECAY = 1000
-    _STOP_EPSILON_DECAY = 4000
+    _STOP_EPSILON_DECAY = 2500
 
     def __init__(self, max_observation_values, min_observation_values, num_of_actions):
         # os is Observation Space
         self._os_space_max = max_observation_values
         self._os_space_min = min_observation_values
+        self._num_of_actions = num_of_actions
 
         discrete_os_steps = [self._AMOUNT_DISCRETE_STATES]*len(max_observation_values)
         self._discrete_os_step_values = (self._os_space_max - self._os_space_min)/discrete_os_steps
@@ -24,15 +24,20 @@ class QTable:
         self._q_table = np.random.uniform(low=-2, high=0, size=(discrete_os_steps + [num_of_actions]))
 
         self._epsilon = self._INITIAL_EPSILON
+        self._epsilon_decay_value = self._INITIAL_EPSILON / (self._STOP_EPSILON_DECAY - self._BEGIN_EPSILON_DECAY)
+
         self._update_count = 0
 
     def get_action(self, current_state):
+        # Introduces randomness to actions (Exploration)
+        if np.random.random() < self._epsilon:
+            return np.random.randint(0, self._num_of_actions)
+
         discrete_state = self._get_discrete_state(current_state)
         q_values = self._q_table[discrete_state]
         action_num = np.argmax(q_values)
 
-        # 0 is move left, 1 is do nothing, 2 is move right
-        return action_num
+        return action_num  # 0 is move left, 1 is do nothing, 2 is move right
 
     def update(self, new_state, new_reward, current_state, current_action):
         self._update_count += 1
@@ -62,6 +67,4 @@ class QTable:
         if self._update_count < self._BEGIN_EPSILON_DECAY or self._update_count > self._STOP_EPSILON_DECAY:
             return
 
-        self._epsilon -= self._EPSILON_DECAY_VALUE
-        if self._epsilon < 0:
-            self._epsilon = 0
+        self._epsilon -= self._epsilon_decay_value
